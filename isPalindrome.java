a23fa761ecc8832655eabd6a03af2f48cc98eeeb
Qiuzi Shangguan <qs2130@vanilla.clic.cs.columbia.edu>
2013-01-02 12:28:58 -0500

    hello

---
+++ isPalindrome.java
@@ -0,0 +1,59 @@
+public static class isPalindrome{
+
+public class LinkedNode{
+
+public int data;
+public LinkedNode next;
+
+}
+	  public static Boolean isPdrome(LinkedNode head,int len){
+
+    	  if(head==null||head.next==null||len==1||len==2&&head.data==head.next.data)
+               return true;
+    	   LinkedNode runner=head;
+    	  int length=len;
+    	  while(length-1>0){
+    	     length--;
+    	     runner=runner.next;
+    	  }
+            if(runner.data==head.data)
+    	    return isPdrome(head.next,len-2);
+              else return false;
+
+    	  }
+
+public static void main(String [] args){
+
+LinkedNode head = new LinkedNode();
+LinkedNode a1= new LinkedNode();
+LinkedNode a2= new LinkedNode();
+LinkedNode a3= new LinkedNode();
+LinkedNode a4= new LinkedNode();
+LinkedNode a5= new LinkedNode();
+LinkedNode a6= new LinkedNode();
+head.data = 1;
+head.next = a1;
+a1.data=2;
+a1.next=a2;
+a2.data=3;
+a2.next=a3;
+a3.data=4;
+a3.next=a4;
+a4.data=3;
+a4.next=a5;
+a5.data=2;
+a5.next=a6;
+a6.data=1;
+a6.next=null;
+LinkedNode runner= head;
+int len =0;
+while(runner!=null){
+   len++;
+   runner=runner.next;
+}
+System.out.println("the linked list is palindrome: "+isPdrome(head,len));
+
+}
+
+
+}
