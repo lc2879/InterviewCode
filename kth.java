6fa613229e5108f09faf4c9cb355dbe2494a951f
Qiuzi Shangguan <qs2130@vanilla.clic.cs.columbia.edu>
2013-01-02 17:38:17 -0500

    hello

---
+++ kth.java
@@ -0,0 +1,35 @@
+public class kth{
+
+   public static class LinkedNode{
+
+	    	  public int data;
+	    	  public LinkedNode next;
+
+	    	  public LinkedNode()
+	    	  {}
+	    	  
+	    	  }
+
+public static LinkedNode thekth(LinkedNode head){
+
+	
+
+
+
+
+
+
+
+
+
+}
+public static void main(String[] args){
+
+
+
+
+}
+
+
+
+}
