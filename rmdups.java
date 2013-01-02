3b4c684c16948c045486b4471302bdc5cbe1511b
Qiuzi Shangguan <qs2130@vanilla.clic.cs.columbia.edu>
2013-01-01 23:27:31 -0500

    helllo

---
+++ rmdups.java
@@ -0,0 +1,30 @@
+public class rmdups{
+
+public class LinkNode{
+
+   private int data;
+   public LinkNode next;
+   public int get()
+  { return data;
+}
+}
+
+
+
+public static void rmDuplicate(linkNode start){
+
+
+
+
+
+}
+
+public static void main(String[] args){
+
+
+
+
+
+}
+
+}
