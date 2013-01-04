d4a348ed39035ead14f64a53343151b62d8ac029
Qiuzi Shangguan <qs2130@vanilla.clic.cs.columbia.edu>
2013-01-03 20:47:10 -0500

    hello

---
+++ comAncester.java
@@ -0,0 +1,16 @@
+public class comAncester{
+
+
+/*with and without parent infomation*/
+public static Node commonAncester(Node v1,Node v2){
+
+  Node target = new Node ();
+
+
+
+
+
+
+
+return target;
+}
---
+++ isBalanced.java
@@ -0,0 +1,9 @@
+public class isBalanced{
+ public static int  isBalanced(Node root){
+         if (root==null) return 0;
+         int lHight,rHight;
+         if((lHight=isBalanced(root.left))==-1||(rHight=isBalanced(root.right))==-1||Math.abs(lHight-rHight)>1)
+                return -1;
+         return Math.max(lHight,rHight)+1;
+}
+}
