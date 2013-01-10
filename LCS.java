4c0db4c916750b7ffbae2a5fd4301c03b827a36a
Qiuzi Shangguan <qs2130@vanilla.clic.cs.columbia.edu>
2013-01-10 00:14:28 -0500

    hello

---
+++ LCS.java
@@ -0,0 +1,11 @@
+/*longest common subsequence*/
+public static String LCS(String str1,String str2){
+
+	String lcs;
+	
+
+
+
+
+return lcs;
+}
---
+++ isWord.java
@@ -0,0 +1,5 @@
+public static Boolean isWord(String word){}
+
+public static Boolean isSplit(String wordList){
+ 	while(int 
+	String pre = wordList.subString(i); 
