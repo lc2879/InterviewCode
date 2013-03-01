d23ded40dfdd7e32f4e4862d1cd8db557fc63cd3
Chang Le <le@dbgpu.(none)>
2013-03-01 01:21:06 -0500

    hello

--- comAncester.java
+++ comAncester.java
@@ -34,3 +34,13 @@ public static Node commonAncester1(Node root,Node v1,Node v2){
       else return left==null ? right:left;
 }
 
+
+Node *LCA(Node *root, Node *p, Node *q) {
+  if (!root) return NULL;
+  if (root == p || root == q) return root;
+  Node *L = LCA(root->left, p, q);
+  Node *R = LCA(root->right, p, q);
+  if (L && R) return root;  // if p and q are on both sides
+  return L ? L : R;  // either one of p,q is on one side OR p,q is not in L&R subtrees
+}
+
