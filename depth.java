c3c80e354d2e0f16a2a9cf5c10aa313668a70bc4
Chang Le <le@dbgpu.(none)>
2013-03-02 21:03:40 -0500

    hello

--- depth.java
+++ depth.java
@@ -2,3 +2,6 @@ public static int depth(Node root,int d){
 	if(root == null) return d-1;
 	else return Math.MAX(depth(root.left, d+1),depth(root.right,d+1);
 }
+
+//easily modified to return the min depth.
+
--- list2BST.java
+++ list2BST.java
@@ -18,4 +18,34 @@ head.head=head.head.next;
 root.right=(list2BST(head,len-(len/2)-1));
 return root;
 }
+/*c++ implementation*/
+BinaryTree* sortedListToBST(ListNode *& list, int start, int end) {
+  if (start > end) return NULL;
+  // same as (start+end)/2, avoids overflow
+  int mid = start + (end - start) / 2;
+  BinaryTree *leftChild = sortedListToBST(list, start, mid-1);
+  BinaryTree *parent = new BinaryTree(list->data);
+  parent->left = leftChild;
+  list = list->next;
+  parent->right = sortedListToBST(list, mid+1, end);
+  return parent;
+}
+ 
+BinaryTree* sortedListToBST(ListNode *head, int n) {
+  return sortedListToBST(head, 0, n-1);
+}
+/*array to bst c++ implementation*/
+BinaryTree* sortedArrayToBST(int arr[], int start, int end) {
+  if (start > end) return NULL;
+  // same as (start+end)/2, avoids overflow.
+  int mid = start + (end - start) / 2;
+  BinaryTree *node = new BinaryTree(arr[mid]);
+  node->left = sortedArrayToBST(arr, start, mid-1);
+  node->right = sortedArrayToBST(arr, mid+1, end);
+  return node;
+}
+ 
+BinaryTree* sortedArrayToBST(int arr[], int n) {
+  return sortedArrayToBST(arr, 0, n-1);
+}
 
