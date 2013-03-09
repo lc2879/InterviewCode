a8a156ef53451f4c1e6c2f834f681001a40f9c72
Chang Le <le@dbgpu.(none)>
2013-03-09 00:42:04 -0500

    hello

---
+++ t.java
@@ -0,0 +1,13 @@
+public class t{
+ public static int i=1;
+ public static void add(int i){
+	i=i+1;
+}
+public static void main(String [] args){
+char a ='a';
+String s= a.toString();
+
+	add(i);
+	System.out.println(s);
+}
+}
