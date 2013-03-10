a43c42a14835c0ebeed1f385de6407327416b987
Chang Le <le@dbgpu.(none)>
2013-03-10 18:05:29 -0400

    hello

--- maze.java
+++ maze.java
@@ -1,4 +1,42 @@
 given a maze (matrix bool[N][M]) where 0 = free way, 1= obsticle - How many ways are there to reach from [0][0] to [N][M]? write a non-recursive solution.
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        int dp[m][n];
+        int val = 1;
+        for(int i = 0; i < m; ++i){
+            if(obstacleGrid[i][0] == 1)
+                val = 0;
+            dp[i][0] = val;
+                
+        }
+        val = 1;
+        for(int i = 0; i < n; ++i){
+            if(obstacleGrid[0][i] == 1){
+                val = 0;
+            }
+            dp[0][i] = val;
+        }
+        for(int i = 1; i < m; ++i){
+            for(int j = 1; j < n; ++j){
+                if(obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
+
+
+
 
 #include <stdio.h>
 #define SIZE 4
