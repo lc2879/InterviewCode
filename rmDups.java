862958aefdbc1e1a60b2accd3c18acee12ba8a3b
Chang Le <le@dbgpu.(none)>
2013-03-12 00:50:56 -0400

    hello

--- rmDups.java
+++ rmDups.java
@@ -49,3 +49,53 @@ public static void main (String [] args){
 
 
 }
+
+
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode newhead = new ListNode(0);
+        ListNode curr=head;
+        ListNode prev = newhead;
+        newhead.next = curr;
+        if(curr==null) return null;
+        while(curr!=null && curr.next!=null){
+            ListNode next = curr.next;
+            if(next.val == curr.val){
+                while(next!=null && next.val == curr.val){
+                    next = next.next;
+                }
+                prev.next = next;
+            }
+            else{
+                prev = curr;
+            }
+            curr = next;
+             
+        }
+        return newhead.next;
+    }
+}
+
+
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode curr=head;
+        if(curr==null) return null;
+        while(curr!=null && curr.next!=null){
+            ListNode next = curr.next;
+            if(curr.next.val == curr.val){
+                curr.next = curr.next.next;
+                continue;
+            }
+            curr = next;
+        }
+        return head;
+    }
+}
+
