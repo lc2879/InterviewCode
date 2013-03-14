19991f5f51b14c0911c7245ef1364317e77031d6
Chang Le <le@dbgpu.(none)>
2013-03-13 22:10:31 -0400

    hello

--- printLevel.java
+++ printLevel.java
@@ -25,3 +25,43 @@ public static void printLevel( Node root){
     }
        
 }
+
+
+print by level bottom up
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        LinkedList<ArrayList<Integer>> reversedList = new LinkedList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> resultList = new ArrayList<ArrayList<Integer>>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        ArrayList<Integer> tempList = new ArrayList<Integer>();
+        if(root == null) return resultList;
+
+        queue.offerLast(root);
+        int numInLayer = 1, numInNextLayer = 0, count = 0;
+        while(!queue.isEmpty()){
+            count ++;
+            TreeNode node = queue.pollFirst();
+            if(node.left != null){
+                queue.offerLast(node.left);   
+                numInNextLayer ++;
+            }
+            if(node.right != null){
+                queue.offerLast(node.right);
+                numInNextLayer ++;   
+            }
+            tempList.add(node.val);
+            if(count == numInLayer){
+                reversedList.addFirst(tempList);
+                tempList = new ArrayList<Integer>();
+                numInLayer = numInNextLayer;
+                numInNextLayer = 0;
+                count = 0;
+            }
+        }
+        for(ArrayList<Integer> list : reversedList) resultList.add(list);
+        return resultList;
+    }
+}
